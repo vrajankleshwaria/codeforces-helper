7b1cfbcffc1b4573f5480ddfbcb64d5ac0f06d77
vrajankleshwaria <vraj2001sa@gmail.com>
2022-04-01 03:03:48 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,23 @@ import { UpcomingContestList } from "./components/pages/UpcomingContestList";
 import { Problems } from "./components/pages/Problems";
 import { UpSolveContestList } from "./components/pages/UpSolveContestList";
 import { Friends } from "./components/pages/Friends";
-import NavBar from "../src/components/navbar/NavBar";
 import ContestProblemList from "./components/pages/ContestProblemList";
-import _ from "lodash";
+import AppSkeleton from "./AppSkeleton/AppSkeleton";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
-        {/* convert to skeleton */}
-        <Routes>
-          <Route exact="true" path="/" element={<Home />} />
-          <Route path="/UpcomingContest" element={<UpcomingContestList />} />
-          <Route path="/upSolve" element={<UpSolveContestList />} />
-          <Route path="/upSolve/:id" element={<ContestProblemList />} />
-          <Route path="/problems" element={<Problems />} />
-          <Route path="/friends" element={<Friends />} />
-        </Routes>
+        <AppSkeleton>
+          <Routes>
+            <Route exact="true" path="/" element={<Home />} />
+            <Route path="/UpcomingContest" element={<UpcomingContestList />} />
+            <Route path="/upSolve" element={<UpSolveContestList />} />
+            <Route path="/upSolve/:id" element={<ContestProblemList />} />
+            <Route path="/problems" element={<Problems />} />
+            <Route path="/friends" element={<Friends />} />
+          </Routes>
+        </AppSkeleton>
       </Router>
     </>
   );
